a8fc4e78d2a6bf3f9b4db1b51ba678d069fbd701
Bienvenu Kitutu <bienvenu.kitutu@gmail.com>
2019-12-21 14:26:50 +0100

    import Log an Callback

--- app/src/main/java/com/nasande/nasande/MainActivity.java
+++ app/src/main/java/com/nasande/nasande/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import pub.devrel.easypermissions.EasyPermissions;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
